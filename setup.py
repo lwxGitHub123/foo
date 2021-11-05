82f839fa97e76c7b6dbde9cb783440f4409abb49
lwxGitHub123 <34017587+lwxGitHub123@users.noreply.github.com>
2021-11-05 16:01:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,13 @@ from setuptools import setup,find_packages
 setup(name='foo',
       version='0.1',
       description='a small example',
-      classifiers=[
-        'Programming Language :: Python',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-    ],
       url='https://github.com/lwxGitHub123/foo/',
       author='liudongbo',
       author_email='350272438@qq.com',
       license='MIT Licence',
       packages=find_packages(),
-      zip_safe=True
+      zip_safe=True,
+      include_package_data=True,
+      platforms="any",
+      install_requires=[]
      )

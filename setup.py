ef0d66b60cc44c4f1e718efddfeab8f1798fa90d
lwxGitHub123 <34017587+lwxGitHub123@users.noreply.github.com>
2021-11-05 14:53:26 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+setup(name='testtest',
+      version='0.1',
+      description='a small example',
+      classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+      url='https://www.python.org/',
+      author='liudongbo',
+      author_email='350272438@qq.com',
+      license='NEU',
+      packages=find_packages(),
+      zip_safe=True
+     )

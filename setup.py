5f474733a1024c8f0b9afc1e9660d5d505555af0
lwxGitHub123 <34017587+lwxGitHub123@users.noreply.github.com>
2021-11-09 09:44:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup,find_packages
 setup(name='foo-pkg-Liudongbo',
-      version='0.2',
+      version='0.3',
       description='a small example',
       url='https://github.com/lwxGitHub123/foo/',
       author='liudongbo',
